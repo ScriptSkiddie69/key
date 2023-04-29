2d214f188f9a021b1a89e5f6587bc948ca0528dc
ScriptSkiddie69 <110344732+ScriptSkiddie69@users.noreply.github.com>
2023-04-29 22:22:39 +0800

    Update pecter.lua

--- pecter.lua
+++ pecter.lua
@@ -1,6 +1,6 @@
 getgenv().yes = "ahh"
 
-if game:GetService("RbxAnalyticsService"):GetClientId() == "6DBDF301-2C26-4746-A318-3A9DA2DBD3A2" then
+if game:GetService("RbxAnalyticsService"):GetClientId() == "6DBDF301-2C26-4746-A318-3A9eDA2DBD3A2" then
   getgenv().yes = "nil"
   else
   print('hi')
