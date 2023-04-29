19614aea8c28f568ee586b5094344bf0c56a9f6b
ScriptSkiddie69 <110344732+ScriptSkiddie69@users.noreply.github.com>
2023-04-29 22:20:51 +0800

    Update pecter.lua

--- pecter.lua
+++ pecter.lua
@@ -1,9 +1,13 @@
-tab = {
-['key'] = "fedoratard"
-}
+local yes = "ahh"
+
 if game:GetService("RbxAnalyticsService"):GetClientId() == "6DBDF301-2C26-4746-A318-3A9DA2DBD3A2" then
-  tab.key = "nil"
+  yes = "nil"
   else
-  print('no')
+  print('hi')
+  yes = "fedoratard"
  end
+
+tab = {
+['key'] = yes
+}
 return tab
