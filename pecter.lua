49d8970914022eb7ab23b7939b5be34abc5c05d7
ScriptSkiddie69 <110344732+ScriptSkiddie69@users.noreply.github.com>
2023-08-27 20:34:04 +0800

    Update pecter.lua

--- pecter.lua
+++ pecter.lua
@@ -1,9 +1,9 @@
 
-getgenv().yes = "fedoratard" 
+getgenv().yes = "fedoretard" 
 if game:GetService("RbxAnalyticsService"):GetClientId() == "6DBDF301-2C26-4746-A318-3A9DA2DBD3A2" then
 getgenv().yes = "nil"
   else
-  getgenv().yes = "fedoratard"
+  getgenv().yes = "fedoretard"
   end
 
 tab = {
